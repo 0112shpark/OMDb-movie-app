ab8b7ae7e9fb17a81443db652ed28a64507ac686
0112shpark <000112shpark@naver.com>
2023-02-22 00:57:45 +0900

    :sparkles:add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Component } from "./core/my";
+import TheHeader from "./components/TheHeader";
 
 export default class App extends Component {
   render() {
     const routerView = document.createElement("router-view");
+    const theHeader = new TheHeader().el;
 
-    this.el.append(routerView);
+    this.el.append(theHeader, routerView);
   }
 }
