c82cd408c5f2ef3c04a8b68a60c517286636763a
0112shpark <000112shpark@naver.com>
2023-02-21 23:40:14 +0900

    :bugs:restore view more button when go backto list page

--- src/components/MovieListMore.js
+++ src/components/MovieListMore.js
@@ -8,6 +8,7 @@ export default class MovieListMore extends Component {
     });
     movieStore.subscribe("pageMax", () => {
       const { page, pageMax } = movieStore.state;
+      console.log("subs:", pageMax);
       if (pageMax > page) {
         this.el.classList.remove("hide");
       } else {
@@ -16,11 +17,16 @@ export default class MovieListMore extends Component {
     });
   }
   render() {
-    this.el.classList.add("btn", "view-more", "hide");
+    console.log(movieStore.state.pageMax);
+    if (!movieStore.state.message) {
+      this.el.classList.add("btn", "view-more");
+    } else {
+      this.el.classList.add("btn", "view-more", "hide");
+    }
     this.el.textContent = "View more..";
 
     this.el.addEventListener("click", async () => {
-      // this.el.classList.add("hide");
+      this.el.classList.add("hide");
       await searchMovies(movieStore.state.page + 1);
     });
   }
