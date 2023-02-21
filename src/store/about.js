a16b8e3642163c34c2f499cdb8207bc4ff162a3d
0112shpark <000112shpark@naver.com>
2023-02-22 01:32:59 +0900

    :sparkels:add about page

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+import { Store } from "../core/my";
+
+export default new Store({
+  photo: "https://avatars.githubusercontent.com/u/66255101?v=4",
+  name: "0112shpark / Park SeongHyeon",
+  email: "000112shpark@naver.com",
+  github: "https://github.com/0112shpark",
+  repository: "https://github.com/0112shpark/OMDb-movie-app",
+});
