1ad7ea8eaf5305179dc2911319872f0666df272b
0112shpark <000112shpark@naver.com>
2023-02-21 23:29:15 +0900

    :sparkles:add movie details

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,11 +1,13 @@
 import { Store } from "../core/my";
 
 const store = new Store({
-  searchText: " ",
+  searchText: "",
   page: 1,
   pageMax: 1,
-  movies: [],
+  movies: [], //movie lists
+  movie: {}, //movie details
   loading: false,
+  message: "Search for the movie title!",
 });
 
 export default store;
@@ -14,13 +16,34 @@ export const searchMovies = async (page) => {
   store.state.page = page;
   if (page === 1) {
     store.state.movies = [];
+    store.state.message = "";
   }
-  const res = await fetch(
-    `https://omdbapi.com?apikey=d55d3038&s=${store.state.searchText}&page=${page}`
-  );
-  // Search 속성의 값만 받아온다.
-  const { Search, totalResults } = await res.json();
-  store.state.movies = [...store.state.movies, ...Search];
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-  store.state.loading = false;
+  try {
+    const res = await fetch(
+      `https://omdbapi.com?apikey=d55d3038&s=${store.state.searchText}&page=${page}`
+    );
+    // 각 속성의 값만 받아온다.
+    const { Search, totalResults, Response, Error } = await res.json();
+    if (Response === "True") {
+      store.state.movies = [...store.state.movies, ...Search];
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+    }
+  } catch (error) {
+    console.log("searchMoviesError:", error);
+  } finally {
+    store.state.loading = false;
+  }
+};
+
+export const getMovieDetails = async (id) => {
+  try {
+    const res = await fetch(
+      `https://omdbapi.com?apikey=d55d3038&i=${id}&plot=full`
+    );
+    store.state.movie = await res.json();
+  } catch (error) {
+    console.log("getMovieDetails error:", error);
+  }
 };
