51ab223ef8911e7e2ac3118fb284bdfe9eabff52
0112shpark <000112shpark@naver.com>
2023-02-27 17:48:28 +0900

    :art:typing done

--- src/components/TheHeader.ts
+++ src/components/TheHeader.ts
@@ -1,6 +1,20 @@
 import { Component } from "../core/my";
 
+interface State {
+  [key: string]: unknown;
+  menus: {
+    name: string;
+    href: string;
+  }[];
+}
+
 export default class TheHeader extends Component {
+  // 할당 선언 => constructor로 만든 state보다 이 class내의 state가 우선순위를 가짐
+  // render함수가 다시 실행될때, constructor이 아닌 여기서 선언된 state사용.
+  // 초기화시, state = {} as State 형식으로 빈 객체로 할당하게 되면
+  // render함수 재실행시 빈 객체를 사용하게됨.
+  // !를 사용해서 초기화를 한 것처럼 하는 할당선언 사용
+  public state!: State;
   constructor() {
     super({
       tagName: "header",
--- src/store/about.ts
+++ src/store/about.ts
@@ -1,6 +1,14 @@
 import { Store } from "../core/my";
 
-export default new Store({
+interface State {
+  photo: string;
+  name: string;
+  email: string;
+  github: string;
+  repository: string;
+}
+
+export default new Store<State>({
   photo: "https://avatars.githubusercontent.com/u/66255101?v=4",
   name: "0112shpark / Park SeongHyeon",
   email: "000112shpark@naver.com",
